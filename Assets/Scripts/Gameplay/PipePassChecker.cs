0f70f43bdf99091ee0481baca2e8a06b5576c146
Eric <erikbc_95@hotmail.com>
2024-10-06 13:52:35 +0200

    Remove unused variable

--- Assets/Scripts/Gameplay/PipePassChecker.cs
+++ Assets/Scripts/Gameplay/PipePassChecker.cs
@@ -1,12 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PipePassChecker : MonoBehaviour
 {
-    private int score;
-    private void OnTriggerEnter2D()
-    {
-        GameManager.Instance.IncreaseScore();
-    }
+	private void OnTriggerEnter2D()
+	{
+		GameManager.Instance.IncreaseScore();
+	}
 }
